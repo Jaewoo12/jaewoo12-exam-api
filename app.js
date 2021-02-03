4b092397167e3b78f7aa5d880f1a3c9ca51cd646
Jaewoo <jaewoo.jeongg@gmail.com>
2021-02-03 17:31:22 +0900

    update 4

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // http://127.0.0.1:3000/
-app.get('/service', (req, res) => res.send('this page for MSA Project test'));
+app.get('/service', (req, res) => {
+    var user_name = req.param('status=on')
+    res.send('The system is running [Status: ON]{' + user_name + '}');
+});
 app.listen(port, () => console.log('Example app listening on port 3000'))

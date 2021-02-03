e2d554f0338bf9369e4804cfe4bb1193ca031f7a
Jaewoo <jaewoo.jeongg@gmail.com>
2021-02-03 17:43:33 +0900

    update6

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000;
 // http://127.0.0.1:3000/
 app.get('/service', (req, res) => {
     var user_name = req.param('status=on')
-    res.send('The system is running [Status: ON]{' + user_name + '}');
+    res.send('The system is running [Status: ON]');
 });
 app.listen(port, () => console.log('Example app listening on port 3000'))
